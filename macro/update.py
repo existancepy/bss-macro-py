ef5d9a24b933967c5a58fa1556255e4cefadf98a
Brydon <121854370+existancepy@users.noreply.github.com>
2023-10-09 11:26:19 +0800

    fixed update feature

--- macro/update.py
+++ macro/update.py
@@ -2,30 +2,25 @@
 import os
 import requests
 import zipfile
-from io import BytesIO
 import shutil
-from distutils.dir_util import copy_tree
+from io import BytesIO
 
 def update():
-    files = [x for x in os.listdir("./") if x[-2:] ==  "py" and  x!= "update.py" ]
-
-    for f in files:
-        os.remove("./{}".format(f))
-
-    try:
-        shutil.rmtree('./images')
-    except:
-        pass
-    filepath = os.getcwd()
+    destination = os.getcwd().replace("/macro","")
+    source = f"{destination}/bss-macro-py-main"
+    print(os.listdir(destination))
+    for f in os.listdir(destination):
+        if "." in f:
+            os.remove(f"{destination}/{f}")
+        else:
+            shutil.rmtree(f"{destination}/{f}")
     req = requests.get('https://github.com/existancepy/bss-macro-py/archive/master.zip')
     zipf= zipfile.ZipFile(BytesIO(req.content))
-    zipf.extractall(filepath)
-
-    newfiles = os.listdir("./bss-macro-py-main")
-    for i in newfiles:
-        if not i in ['images','theme']:
-            shutil.copyfile("./bss-macro-py-main/{}".format(i), "./{}".format(i))
-        else:
-            copy_tree("./bss-macro-py-main/{}".format(i), "./{}".format(i))
-    shutil.rmtree('./bss-macro-py-main')
-    print('Update complete. You can now relaunch the macro')
+    zipf.extractall(destination)
+    files = os.listdir(source) 
+    for file in files: 
+        file_name = os.path.join(source, file) 
+        shutil.move(file_name, destination) 
+    print("Files Moved")
+    shutil.rmtree(source)
+    print("Update complete. You can now relaunch the macro")
