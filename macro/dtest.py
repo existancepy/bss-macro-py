1072c6465a3b533d8eba6a125bc25d732111daa0
scratchusernamemrtbts <66320961+scratchusernamemrtbts@users.noreply.github.com>
2023-11-02 19:27:02 +0700

    supports slash command

--- macro/dtest.py
+++ macro/dtest.py
@@ -11,17 +11,10 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command(name="rejoin",description="rejoin the game")
+async def rejoincmd(ctx):
+    ctx.channel.send("Now attempting to rejoin")
+    rejoin()
 
-    if message.content.startswith('bsm'):
-        args = message.content.split(" ")[1:]
-        cmd = args[0]
-        if cmd.lower() == "rejoin":
-            message.channel.send("Now attempting to rejoin")
-            rejoin()
-        
 
 client.run(setdat['discord_bot_token'])
