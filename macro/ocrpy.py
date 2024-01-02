9a891a034520a11a00aa64537659ac1ed55ad1a6
existancepy <121854370+existancepy@users.noreply.github.com>
2024-01-03 04:45:32 +0800

    Update ocrpy.py

--- macro/ocrpy.py
+++ macro/ocrpy.py
@@ -49,11 +49,11 @@ def imToString(m):
         cap = pag.screenshot(region=(ww*3//4, 0, ww//4,wh//3))
     elif m == "ebutton":
         cap = pag.screenshot(region=(ww//(2.65*xsm),wh//(20*ysm),ww//(21*xlm),wh//(17*ylm)))
-        cap.save("{}.png".format(sn))  
+        cap.save("{}.png".format(sn))
         result = ocr.ocr("{}.png".format(sn),cls=False)[0]
-        result = sorted(result, key = lambda x: x[1][1], reverse = True)
         os.remove("{}.png".format(sn))
         try:
+            result = sorted(result, key = lambda x: x[1][1], reverse = True)
             return result[0][1][0]
         except:
             return ""
