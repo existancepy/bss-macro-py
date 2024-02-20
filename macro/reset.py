60a3274d32c8eb83f4374c6d96f1780f74dee2ab
existancepy <121854370+existancepy@users.noreply.github.com>
2024-02-20 19:25:12 +0800

    Update reset.py

--- macro/reset.py
+++ macro/reset.py
@@ -27,7 +27,7 @@ def loadSave():
             l[1] = int(l[1])
         savedata[l[0]] = l[1]
 
-def reset():
+def reset(hiveCheck=False):
     setdat = loadsettings.load()
     loadSave()
     rhd = setdat["reverse_hive_direction"]
@@ -94,68 +94,9 @@ def reset():
             time.sleep(0.5)
         time.sleep(1)
     return False
-    webhook("Notice","Hive not found. Assume that player is facing the right direction","red",1)
-
-def resetCheck():
-    
-    loadSave()
-    ysm = loadsettings.load('multipliers.txt')['y_screenshot_multiplier']
-    xsm = loadsettings.load('multipliers.txt')['x_screenshot_multiplier']
-    for _ in range(2):
-        webhook("","Resetting character","dark brown")
-        pag.moveTo(mw/(4.11*xsm),mh/(9*ysm))
-        ww = savedata["ww"]
-        wh = savedata["wh"]
-        xo = ww//4
-        yo = wh//100*90
-        xt = xo*2
-        yt = wh//100*20
-        time.sleep(2)
-        pag.press('esc')
-        time.sleep(0.1)
-        pag.press('r')
-        time.sleep(0.2)
-        pag.press('enter')
-        sleep(8.5)
-        for _ in range(4):
-            pag.press('pgup')
-        time.sleep(0.1)
-        for _ in range(6):
-            pag.press('o')
-        #im = pag.screenshot(region = (xo,yo,xt,yt))
-        #im.save('a.png')
-
-        time.sleep(0.4)
-        for _ in range(4):
-            r = getPixelColor(ww//2,wh-2)
-            passed = 1
-            for i in range(len(tar)):
-                if tar[i]-var <= r[i] <= tar[i]+var:
-                    pass
-                elif i == 2 and r[2] < 15:
-                    pass
-                else:           
-                    passed = 0
-                    break
-                
-                
-            if passed:
-                time.sleep(0.1)
-                for _ in range(4):
-                    pag.press(".")
-
-                time.sleep(0.1)
-                for _ in range(4):
-                    pag.press('pgdn')
-                return True
-            for _ in range(4):
-                pag.press(",")
-            
-            time.sleep(0.5)
-        time.sleep(1)
-    for _ in range(4):
-        pag.press(",")
-    webhook("Notice","Hive not found.","red",1)
-    return False
+    if hiveCheck:
+        webhook("Notice","Hive not found.","red",1)
+    else:
+        webhook("Notice","Hive not found. Assume that player is facing the right direction","red",1)
 
 
