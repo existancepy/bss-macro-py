bcf407845b60f8102747a3f8590389dce72c36c1
existancepy <121854370+existancepy@users.noreply.github.com>
2024-07-19 16:10:48 +0800

    Update ocrpy.py

--- macro/ocrpy.py
+++ macro/ocrpy.py
@@ -7,6 +7,7 @@ import os
 import subprocess
 import time
 import mss
+import mss.tools
 
 useOCRMac = False
 try:
@@ -31,12 +32,16 @@ def loadRes():
     return outdict
 
 mw, mh = pag.size()
-def mssScreenshot(x,y,w,h):
+def mssScreenshot(x,y,w,h, saveName = "honey.png"):
     with mss.mss() as sct:
         # The screen part to capture
         monitor = {"left": x, "top": y, "width": w, "height": h}
         # Grab the data and convert to pillow img
         sct_img = sct.grab(monitor)
+        if saveName:
+            # Save to the picture file
+            mss.tools.to_png(sct_img.rgb, sct_img.size, output=saveName)
+
         img = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
         return img
 
